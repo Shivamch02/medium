8c711c0885bfa5a08155e627378213fa92fd5481
Shivam Chaurasiya <chshivam53@gmail.com>
2024-11-17 01:57:20 +0530

    Database Setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,77 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
+import { sign, verify } from "hono/jwt";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.use("/api/v1/blog/*", async (c, next) => {
+  const jwt = c.req.header("Authorization");
+  if (!jwt) {
+    c.status(401);
+    return c.json({ error: "unauthorized" });
+  }
+  const token = jwt.split(" ")[1];
+  const payload = await verify(token, c.env.JWT_SECRET);
+  if (!payload) {
+    c.status(401);
+    return c.json({ error: "unauthorized" });
+  }
+  c.set("userId", payload.id);
+  await next();
 });
 
-app.post("/api/v1/signup", (c) => {
-  return c.json({ msg: "signup" });
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+    const token = await sign(
+      {
+        id: user.id,
+      },
+      c.env.JWT_SECRET
+    );
+    return c.json({ jwt: token });
+  } catch (e) {
+    return c.json({ error: "Invalid email or password" });
+  }
 });
 
-app.post("/api/v1/login", (c) => {});
+app.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+    },
+  });
+
+  if (!user) {
+    c.status(403);
+    return c.json({ error: "user not found" });
+  }
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({ jwt });
+});
+
 app.post("/api/v1/blog", (c) => {});
 app.put("/api/v1/blog", (c) => {});
 app.get("/api/v1/blog/:id", (c) => {});
